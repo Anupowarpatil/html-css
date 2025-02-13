a2094b642ea8ca78746c2c7321878a2d22af79c6
Purva Kiran Powar Patil <109520470+Anupowarpatil@users.noreply.github.com>
2025-02-13 10:37:28 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function addition() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    alert("Addition is "+result);
+}
+
+function subtraction() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    alert("subtraction is "+result);
+}
+
+function multiplication() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 * num2;
+    alert("Multiplication  is "+result);
+}
+
+function division() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    if (num2 !== 0) {
+        var result = num1 / num2;
+        alert("Division is "+result);
+    } else {
+        alert("Cannot divide by zero!");
+    }
+}
